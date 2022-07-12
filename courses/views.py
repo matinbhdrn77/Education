e050cd2588152f3dbbfaa53646c52a8840d44080
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-07-12 18:13:15 +0430

    add permissions

--- courses/views.py
+++ courses/views.py
@@ -35,13 +35,13 @@ class ManageCourseListView(OwnerCourseMixin, ListView):
     permission_required = 'courses.view_course'
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    permission_required = 'courses.view_course'
+    permission_required = 'courses.add_course'
 
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    permission_required = 'courses.view_course'
+    permission_required = 'courses.change_course'
 
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
-    permission_required = 'courses.view_course'
+    permission_required = 'courses.delete_course'
