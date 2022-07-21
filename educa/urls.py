3e98b144a0e5fac70bb66bc7feae465159b76b2b
matinbhdrn77 <matinbhdrn77@gmail.com>
2022-07-21 10:39:28 +0430

    configure media folder

--- educa/urls.py
+++ educa/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('course/', include('courses.urls')),
     path('students/', include('students.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
